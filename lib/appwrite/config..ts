9ea9aa1d97a665f69a8afa766ae9ef4feb0a7607
LucasLlusco <llusco.lucas2@gmail.com>
2025-03-27 21:30:03 -0300

    install and configure Appwrite for authentication

---
+++ lib/appwrite/config..ts
@@ -0,0 +1,34 @@
+"use server"
+import { Client, Databases, Account } from 'node-appwrite'
+
+export const createSessionClient = async (session:string) => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!) 
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!); 
+
+  if (session) {
+    client.setSession(session);
+  }
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export const createAdminClient = async () => {
+  const client = new Client()
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+  .setKey(process.env.NEXT_APPWRITE_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() { 
+      return new Databases(client);
+    },
+  };
+}
